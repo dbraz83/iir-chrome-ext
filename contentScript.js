99f0ac0ee25496961b23a0b12e34e5692da4d945
John Watson <johnwatson484@hotmail.com>
2018-06-13 20:12:47 +0100

    Updated contentScript to store search array in local storage.
    Wrapped contentScript call stack in document ready method.
    
    Refactored link array updates to only include valid links.

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,59 @@
+$(document).ready(function () {
+    // added this as the checkCookie function didn't seem to trigger
+    checkCookie();
+
+    // use userid and link to the query and SERP results that are input and returned
+    const userID = getCookie("username");
+
+    console.log("UserID is " + userID);
+    console.log(userID + " QUERY: " + $("#lst-ib").val());
+
+    /*
+    Reads the page for the query that was submit.
+    finds all search results and creates an array of the URL's. 
+    */
+
+    // Are there any g class divs?
+    if ($(".g").length > 0) {
+
+        var searchResult = [];
+
+        // For each one, extract the link text if link and text are present.
+        $(".g").each(function () {
+            var link = $(this).find("h3.r").find("a").attr('href');
+            var text = $(this).find("h3.r").find("a").text();
+            if (link != null && text != null) {
+                searchResult.push(link + " | " + text);
+            }
+        });
+
+        //Save array in local storage using search criteria as part of key. 
+        //If local storage for search already exists add links to existing array
+        var searchResultKey = 'searchArray-' + $("#lst-ib").val();
+
+        if (localStorage.getItem(searchResultKey) === null) {
+            localStorage.setItem(searchResultKey, JSON.stringify(searchResult));
+        }
+        else {
+            var existingSearchResult = JSON.parse(localStorage.getItem(searchResultKey));
+
+            searchResult.forEach(function (item) {
+                if (existingSearchResult.indexOf(item) === -1) {
+                    existingSearchResult.push(item);
+                }
+            });
+
+            localStorage.setItem(searchResultKey, JSON.stringify(existingSearchResult));
+        }
+
+        //Call array from local storage and display in console for reference.
+        console.log(JSON.parse(localStorage.getItem(searchResultKey)));
+    }
+
+    logCurrentUrl();
+    window.addEventListener('click', handleWindowClick, { passive: true });
+});
+
 // can localStorage be utilised?
 /*
 function storageAvailable(type) {
@@ -32,144 +88,89 @@ console.log(storageAvailable('sessionStorage'));
 chrome.storage.local.get('value', function(result) {
           console.log('Username currently is ' + result);
         });
-	*/	
+	*/
 //const userID = Math.random().toString(36).substr(2, 6);
 //console.log(userID);
 
 // set cookie as session ID. Allows for username to be used over multiple tabs 
 // courtesy of https://www.w3schools.com/js/js_cookies.asp
 
-function setCookie(cname, cvalue, exdays){
- var d = new Date();
- d.setTime(d.getTime() + (exdays*24*60*60*1000));
- var expires = "expires="+ d.toUTCString();
- document.cookie = "username" + "=" + cvalue + ";" + expires + ";path=/";
- console.log("cookie name " + cname);
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = "username" + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("cookie name " + cname);
 
 }
 
-function getCookie(cname){
-	var name = cname + "=";
-		var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(';');
-	for(var i = 0; i<ca.length; i++){
-	var c = ca[i];
-		while (c.charAt(0) == ' '){
-		c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-		return c.substring(name.length,c.length);
-		}
-	}
-	return "";
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 function checkCookie() {
     var user = getCookie("username");
-	var expired = getCookie("exdays");
-	//console.log("expiry is " + expired);
+    var expired = getCookie("exdays");
+    //console.log("expiry is " + expired);
     if (user.length === 8 && expired != null) {
         console.log("Welcome again " + user);
     } else {
-		// username was a random 6 string which lasts for 1 day - intitally was random but risks duplication, 
-	    // instead opted for hashing the date, howevere creates an 8 digit string not 6
-    var date = (+new Date).toString(36).slice(3,8);
-    var str = Math.random().toString(36).substr(2, 3); 
-   //var res = date.slice(3,8);
-   var user = str+date;
+        // username was a random 6 string which lasts for 1 day - intitally was random but risks duplication, 
+        // instead opted for hashing the date, howevere creates an 8 digit string not 6
+        var date = (+new Date).toString(36).slice(3, 8);
+        var str = Math.random().toString(36).substr(2, 3);
+        //var res = date.slice(3,8);
+        var user = str + date;
         if (user.length === 8 && user != null) {
             setCookie("username", user, 1);
         }
-    } 
-	
-}
-
-
-// added this as the checkCookie function didn't seem to trigger
-checkCookie();
-
-//var user = (+new Date).toString(36);
-//console.log(document.cookie);
-
-// for testing purposes, and also to hard reset the cookie username
-//document.cookie = "username=Jasd; expires=Thu, 08 June 2018 18:27:00 UTC; path=/";
-/*
-if (document.cookie.split(';').filter((item) => {
-    return item.includes('username=')
-}).length) {
-    console.log('The cookie "reader" exists')
-}
-else{
-	console.log("No cookie");
-}
-
-var x = document.cookie;
-console.log("doc cookie" + x);
-*/
-
-
-// use userid and link to the query and SERP results that are input and returned
-const userID = getCookie("username");
-
-console.log("UserID is " + userID);
-console.log(userID + " QUERY: " + $("#lst-ib").val() );
-
-/*
-
-Reads the page for the query that was submit.
-finds all search results and creates an array of the URL's. 
-
-*/
-// Are there any g class divs?
-if($(".g").length > 0) {
-
-// For each one, extract the link text and send it to the console
-
-$( ".g" ).each( function( index, element ){
-    let searchResult = [];
-	 
-	 var d = new Date();
-	 //var d = new Date().toLocaleTimeString();
-    //var n = d.toUTCString();
-
-	searchResult.push($( this ).find("h3.r").find("a").attr('href') + " | " + $( this ).find("h3.r").find("a").text()); 
-	// console.log(userID + " RESULT: " + $( this ).find("h3.r").find("a").attr('href') + " | " + $( this ).find("h3.r").find("a").text() );
-	//console.log(searchResult)
-
-});
-}
-
-/*
-Checks location of URL every 5 seconds, this will be reduced to every 1/2 second once live, this is in order to track user navigation
-*/
-
- window.onpopstate = function(event) {
-  console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
-};
- window.onhashchange = function() { 
-    console.log("Currently URL is " + window.location.href);
- }
- setInterval(function(){ 
- 
- console.log("Currently URL is " + window.location.href); }, 5000);
- 
- // SERP link click log
-// listens for SERP link click
-// AIM IS TO COMPARE AGAINST All SERP links in array (above) to get SERP rank
-
-function handleWindowClick(event) {
-            var origEl = event.target;
-            if(origEl.tagName == 'A') {
-                // do some things with event link: origEl.toString();
-                console.log("Event Link: " + origEl.toString());
-            } else if(origEl.parentNode.tagName == 'A') {   
-                console.log("SERP click:" + origEl.parentNode.toString());
-            } else if(origEl.tagName == 'SPAN') {
-                console.log("Span  is clicked"); 
-            }
     }
 
-window.addEventListener('click', handleWindowClick, false);
+}
+
+function logCurrentUrl() {
+    /*
+    Checks location of URL every 5 seconds, this will be reduced to every 1/2 second once live, this is in order to track user navigation
+    */
+    window.onpopstate = function (event) {
+        console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+    };
+    window.onhashchange = function () {
+        console.log("Currently URL is " + window.location.href);
+    }
+    setInterval(function () {
+
+        console.log("Currently URL is " + window.location.href);
+    }, 5000);
+};
+
+
+// SERP link click log
+// listens for SERP link click
+// AIM IS TO COMPARE AGAINST All SERP links in array (above) to get SERP rank
+function handleWindowClick(event) {
+    var origEl = event.target;
+    if (origEl.tagName == 'A') {
+        // do some things with event link: origEl.toString();
+        console.log("Event Link: " + origEl.toString());
+    } else if (origEl.parentNode.tagName == 'A') {
+        console.log("SERP click:" + origEl.parentNode.toString());
+    } else if (origEl.tagName == 'SPAN') {
+        console.log("Span  is clicked");
+    }
+}
 
 // tracks when URL hash changes, however not all URLS use hash....
 //see above for URL following
